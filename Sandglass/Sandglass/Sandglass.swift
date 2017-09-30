57516eb2b8d3d9dc3c6742f5387597189514ab87
Bruce Jackson <zysios@foxmail.com>
2017-09-30 18:08:17 +0800

    add base timer function

--- Sandglass/Sandglass/Sandglass.swift
+++ Sandglass/Sandglass/Sandglass.swift
@@ -7,6 +7,74 @@ import Foundation
 
 class Sandglass {
 
-    
+    private(set) var running = false
+
+    private let timerSource: DispatchSource
+
+    class func scheduleOneshot(interval: TimeInterval,
+                               queue: DispatchQueue = .main,
+                               action: @escaping () -> ()) -> Sandglass {
+        let timer = Sandglass(interval: interval, queue: queue, action: action)
+        timer.start()
+
+        return timer
+    }
+
+    class func scheduleRepeating(interval: TimeInterval,
+                                 queue: DispatchQueue = .main,
+                                 action: @escaping () -> ()) -> Sandglass {
+        let timer = Sandglass(interval: interval, repeats: true, queue: queue, action: action)
+        timer.start()
+
+        return timer
+    }
+
+    init(interval: TimeInterval, repeats: Bool = false, queue: DispatchQueue = .main, action: @escaping () -> ()) {
+        timerSource = DispatchSource.makeTimerSource(queue: queue) as! DispatchSource
+        timerSource.setEventHandler(handler: action)
+
+        if !repeats {
+            timerSource.scheduleOneshot(deadline: .now() + interval)
+        } else {
+            timerSource.scheduleRepeating(deadline: .now() + interval, interval: interval)
+        }
+    }
+
+    func start() {
+        guard !running else {
+            return
+        }
+
+        if #available(iOS 10.0, *) {
+            timerSource.activate()
+        } else {
+            timerSource.resume()
+        }
+
+        running = true
+    }
+
+    func suspend() {
+        timerSource.suspend()
+        running = false
+    }
+
+    func resume() {
+        guard !running else {
+            return
+        }
+
+        timerSource.resume()
+        running = true
+    }
+
+    func cancel() {
+        timerSource.cancel()
+        running = false
+    }
+
+    deinit {
+        print("Timer dealloc")
+    }
 
 }
